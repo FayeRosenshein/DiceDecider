cabca6fcfe8142b4ec7e4433b7e1f666166699c1
Faye Rosenshein <rosefaye.a@gmail.com>
2023-02-28 14:40:43 -0700

    Add some detailing to page

--- src/Components/CharacterCard/CharacterCard.js
+++ src/Components/CharacterCard/CharacterCard.js
@@ -1,5 +1,5 @@
 import './CharacterCard.css'
 
 export default function CharacterCard() {
-	
+	// displayed a character card with name and img
 }
--- src/Components/Classes/Classes.js
+++ src/Components/Classes/Classes.js
@@ -1,5 +1,5 @@
 import './Classes.css'
 
 export default function Classes() {
-	
+	// display all characterCards
 }
